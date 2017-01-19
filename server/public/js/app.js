a03d54e1d3a6d6b9759d3808440fc78e8e332486
qfdk <qfdk@vip.qq.com>
2017-01-19 18:27:14 +0100

    [Update] add js for scoring

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -11,17 +11,28 @@ $('#search').on('click', function (e) {
   e.preventDefault()
   var tmp = document.querySelector('form input[name=url]').value
   $.get("http://localhost:8983/solr/corejouve/select?indent=on&q=" + tmp + "&wt=json", function (data) {
-    var cpt = 1
     var json = JSON.parse(data)
     var docs = json.response.docs
-    docs.forEach(function (doc) {
+    docs.forEach(function (doc, index) {
+      index++
       var id = doc.id
       var name = doc.name_txt_en
       var des = doc.description_txt_en
-      var note= '<span class="glyphicon glyphicon-star" aria-hidden="true"></span>'
-      var tr = '<tr class="active"><th scope="row">' + cpt + '</th><td>' + id + '</td><td>' + name + '</td><td>'+des+'</td><td>'+note+'</td></tr>'
+      var note = '<div id="target' + index + '"></div><div id="hint' + index + '"></div>'
+      var tr = '<tr class="active"><th scope="row">' + index + '</th><td>' + id + '</td><td>' + name + '</td><td>' + des + '</td><td>' + note + '</td></tr>'
       $('tbody').append(tr)
-      cpt++
+      $('#target' + index).raty({
+        cancelHint: 'none',
+        hints: ['Not relevant', 'Fair', 'Good', 'Excellent', 'Perfect'],
+        start: 0,
+        number: 5,
+        click:function(score,e){
+          // alert(score-1)
+        },
+        target: '#hint' + index,
+        starOff: 'imgs/star-off.png',
+        starOn: 'imgs/star-on.png'
+      })
     }, this);
   })
 })
