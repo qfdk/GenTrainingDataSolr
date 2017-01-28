4dd4ceaf233735333a4c4dc86384b3566145fb64
qfdk <qfdk@vip.qq.com>
2017-01-29 00:07:38 +0100

    [Update] version vue.js

---
+++ webUI/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container" id="app">
+    <top></top>
+    <index></index>
+    <bot></bot>
+  </div>
+</template>
+
+<script>
+import top from './components/Header'
+import index from './components/Index'
+import bot from './components/Footer'
+
+export default {
+  name: 'app',
+  components: {
+    top,
+    index,
+    bot
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ webUI/src/components/Jumbotron.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="jumbotron">
+    <div class="container">
+      <h1>GenTrainingDataSolr</h1>
+      <p>A simple tool to generate training data for Apache Solr</p>
+      <pre># To enable LTR</br>> solr start -Dsolr.ltr.enabled=true</pre>
+      <a class="btn btn-primary" href="conf/solrconfig.xml">solrconfig.xml</a>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "jumbotron",
+  data(){
+    return {
+      //Component Data
+      example: true,
+      foo: "bar"
+    }
+  },
+  methods: {
+    functionExample(){
+      //Basic Component method
+    }
+  }
+}
+</script>
