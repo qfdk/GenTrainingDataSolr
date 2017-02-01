944f989ed49e5adc29d01b376607496fcd628b50
qfdk <qfdk@vip.qq.com>
2017-02-01 16:47:15 +0100

    [fix] storage

--- web/src/storage.js
+++ web/src/storage.js
@@ -1,8 +1,9 @@
+const STORAGE_KEY='ltr'
 export default {
-  get: function (STORAGE_KEY) {
+  get: function (STORAGEKEY) {
     return JSON.parse(window.localStorage.getItem(STORAGE_KEY) || '')
   },
-  set: function (STORAGE_KEY,data) {
+  set: function (data) {
     window.localStorage.setItem(STORAGE_KEY, JSON.stringify(data))
   }
 }
